52eacdb152a25a41b81538a4382415601bf03da2
ShikshaFinder <janiharsh794@gmail.com>
2024-05-07 19:38:11 +0530

    error toast

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -56,6 +56,17 @@ export default function Login() {
         password,
       });
 
+      if (error) {
+        toast({
+          title: "Error.",
+          description: error.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+        return;
+      }
+
       setTimeout(() => {
         router.reload();
       }, 2000);
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -54,6 +54,17 @@ export default function SignupCard() {
           },
         },
       });
+      if (error) {
+        toast({
+          title: "Error",
+          description: error.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+        return;
+      }
+
       router.push("/checkmail");
     } catch (error) {
       console.log(error);
