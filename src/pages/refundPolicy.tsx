df6bc172cbb713bb810918e22966a87800284e84
ShikshaFinder <janiharsh794@gmail.com>
2024-07-02 17:15:01 +0530

    policy

---
+++ src/pages/refundPolicy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function refundPolicy() {
+  return (
+    <div>This cancellation policy outlines about how you can cancel or seek a refund for a product / service that you have purchased through the Platform. Under this policy:Cancellations will only be considered if the request is made 20  days of placing the order. However, cancellation request may not be entertained if the orders have been communicated to such sellers / merchant(s) listed on the Platform and they have initiated the process of shipping them, or the product is out for delivery. In such an even, you may choose to reject the product at the doorstep. shikshafinder does not accept cancellation requests for perishable items like flowers, eatables, etc. However, the refund / replacement can be made if the user establishes that quality of the product delivered is not good.In case of receipt of damaged or defective items, please report to our customer service team. The request would be entertained once the seller/ merchant listed on the Platform, has checked and determined the same at its own end. This should be reported within  20  days of receipt of products. In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within  20  of receiving the product. The customer service team after looking into your complaint will take an appropriate decision.In case of complaints regarding the products that come with a warranty from the manufacturers, please refer the issue to them.In case of any refunds approved by  shikshafinder , it will take10 days for the refund to he processed to you.</div>
+  )
+}
+
+export default refundPolicy
